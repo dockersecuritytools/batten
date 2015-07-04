f415274e360379971faffc3a919af4756c4debf8
Amir Jerbi <jerbia@gmail.com>
2015-07-04 23:51:37 +0300

    added usage for remote scan

--- batten.go
+++ batten.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"os"
-	"io"
-	"io/ioutil"
 	"fmt"
-	"log"
-	"./batten" 
-	"./cli"
+	"github.com/dockersecuritytools/batten/batten"
+	"github.com/dockersecuritytools/batten/cli"
+	"github.com/Sirupsen/logrus"
 	"gopkg.in/alecthomas/kingpin.v1"
-	docker "github.com/fsouza/go-dockerclient"
 )
 
 const (
 	Name        = "batten"
 	Description = "Hardening and Auditing Tool For Docker Hosts & Containers"
 	Version     = "0.1.0"
-	BattenDockerRepository = "jerbi/batten"
 )
 
 var (
@@ -31,111 +26,18 @@ var (
 	appCheck = app.Command("check", "Check host for known issues.")
 )
 
-var (
-    Trace   *log.Logger
-    Info    *log.Logger
-    Warning *log.Logger
-    Error   *log.Logger
-)
-
-func initLogs(
-    traceHandle   io.Writer,
-    infoHandle    io.Writer,
-    warningHandle io.Writer,
-    errorHandle   io.Writer) {
-
-    Trace = log.New(traceHandle,
-        "TRACE: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Info = log.New(infoHandle,
-        "INFO: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Warning = log.New(warningHandle,
-        "WARNING: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Error = log.New(errorHandle,
-        "ERROR: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+func fatalf(format string, args ...interface{}) {
+	fmt.Printf("* fatal: "+format+"\n", args...)
+	os.Exit(1)
 }
 
-func newDockerClient() (*docker.Client, error) {
-	if len(*tlscert) == 0 {
-		return docker.NewClient(*serverIP)
-	} else {
-		return docker.NewTLSClient(*serverIP, *tlscert, *tlskey, *tlscacert)
-	}
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetOutput(os.Stderr)
 }
 
-func remoteCheck() (error){
-	
-	client, err := newDockerClient()
-	
-	if err != nil {
-		Error.Fatalf("Failed to connect to host '%s'. Error: %s", *serverIP, err)
-	}
-	
-	// Pull Batten image from the Docker repository
-	var buf bytes.Buffer
-	err = client.PullImage(docker.PullImageOptions{Repository: BattenDockerRepository, OutputStream: &buf}, docker.AuthConfiguration{})
-	if err != nil {
-		Error.Fatalf("Failed to pull '%s' image on host '%s'. Error: %s", BattenDockerRepository, *serverIP, err)
-	}
-	
-	// Create Batten container
-	config := &docker.Config{Image: BattenDockerRepository}
-	container, err := client.CreateContainer(docker.CreateContainerOptions{Config: config})
-	if err != nil {
-		return err
-	}
-			
-	// Start Batten container
-	binds := []string{"/var/run/docker.sock:/var/run/docker.sock"}
-	hostConfig := &docker.HostConfig{Binds: binds}
-	err = client.StartContainer(container.ID, hostConfig)
-	if err != nil {
-		return err
-	}	
-	
-	// Wait for container scan to finish
-	code, err := client.WaitContainer(container.ID)
-	
-	if err != nil {
-		return err
-	}
-	
-	if code != 0 {
-		fmt.Printf("container run returned %d", code )	
-	}
-	
-	// Print container logs
-	opts := docker.AttachToContainerOptions{
-		Container:    container.ID,
-		OutputStream: &buf,
-		Stdout:       true,
-		Stderr:       true,
-		Logs:         true,
-	}
-	err = client.AttachToContainer(opts)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%s", buf.String())
-	
-	// Cleanup - remove container and image
-	err = client.RemoveContainer(docker.RemoveContainerOptions{ID: container.ID})
-	if err != nil {
-		err = client.RemoveImage(BattenDockerRepository)
-	}
-	
-	return err
-}
 
 func main() {
-	initLogs(ioutil.Discard, os.Stdout, os.Stdout, os.Stdout)
-		
 	kingpin.Version(Version)
 	args, err := app.Parse(os.Args[1:])
 	
@@ -152,5 +54,4 @@ func main() {
 	default:
 		app.Usage(os.Stdout)
 	}
-
 }
